5b15fc97f3af406e7eb57756eced6260a1d2ca33
Shikha Varshney <varshneydheeraj46@gmail.com>
2026-07-24 10:08:21 +0530

    Add show hide password in signup

--- static/js/signup.js
+++ static/js/signup.js
@@ -27,3 +27,24 @@ form.addEventListener("submit", async (e) => {
     alert(data.message);
 
 });
+
+function togglePassword() {
+
+    var password = document.getElementById("password");
+    var eye = document.getElementById("eyeIcon");
+
+    if (password.type === "password") {
+
+        password.type = "text";
+        eye.classList.remove("fa-eye");
+        eye.classList.add("fa-eye-slash");
+
+    } else {
+
+        password.type = "password";
+        eye.classList.remove("fa-eye-slash");
+        eye.classList.add("fa-eye");
+
+    }
+
+}
